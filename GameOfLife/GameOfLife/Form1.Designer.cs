eadcc109eb5544e1706d32837ba608e961f7501c
Geri902 <andigergo2003@gmail.com>
2023-09-17 16:18:43 +0200

    Kész a keretrendszer a képeknek.

--- GameOfLife/GameOfLife/Form1.Designer.cs
+++ GameOfLife/GameOfLife/Form1.Designer.cs
@@ -28,13 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pan_keret = new System.Windows.Forms.Panel();
+            this.tb_sorok = new System.Windows.Forms.TextBox();
+            this.tb_oszlopok = new System.Windows.Forms.TextBox();
+            this.lb_sorok = new System.Windows.Forms.Label();
+            this.lb_oszlopok = new System.Windows.Forms.Label();
+            this.btn_generate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // pan_keret
+            // 
+            this.pan_keret.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pan_keret.Location = new System.Drawing.Point(3, 37);
+            this.pan_keret.Name = "pan_keret";
+            this.pan_keret.Size = new System.Drawing.Size(631, 315);
+            this.pan_keret.TabIndex = 0;
+            // 
+            // tb_sorok
+            // 
+            this.tb_sorok.Location = new System.Drawing.Point(66, 11);
+            this.tb_sorok.Name = "tb_sorok";
+            this.tb_sorok.Size = new System.Drawing.Size(100, 20);
+            this.tb_sorok.TabIndex = 0;
+            // 
+            // tb_oszlopok
+            // 
+            this.tb_oszlopok.Location = new System.Drawing.Point(328, 11);
+            this.tb_oszlopok.Name = "tb_oszlopok";
+            this.tb_oszlopok.Size = new System.Drawing.Size(100, 20);
+            this.tb_oszlopok.TabIndex = 1;
+            // 
+            // lb_sorok
+            // 
+            this.lb_sorok.AutoSize = true;
+            this.lb_sorok.Location = new System.Drawing.Point(22, 15);
+            this.lb_sorok.Name = "lb_sorok";
+            this.lb_sorok.Size = new System.Drawing.Size(38, 13);
+            this.lb_sorok.TabIndex = 2;
+            this.lb_sorok.Text = "Sorok:";
+            // 
+            // lb_oszlopok
+            // 
+            this.lb_oszlopok.AutoSize = true;
+            this.lb_oszlopok.Location = new System.Drawing.Point(268, 14);
+            this.lb_oszlopok.Name = "lb_oszlopok";
+            this.lb_oszlopok.Size = new System.Drawing.Size(54, 13);
+            this.lb_oszlopok.TabIndex = 3;
+            this.lb_oszlopok.Text = "Oszlopok:";
+            // 
+            // btn_generate
+            // 
+            this.btn_generate.Location = new System.Drawing.Point(467, 9);
+            this.btn_generate.Name = "btn_generate";
+            this.btn_generate.Size = new System.Drawing.Size(165, 22);
+            this.btn_generate.TabIndex = 4;
+            this.btn_generate.Text = "Keretrendszer generálása";
+            this.btn_generate.UseVisualStyleBackColor = true;
+            this.btn_generate.Click += new System.EventHandler(this.btn_generate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(910, 626);
+            this.Controls.Add(this.btn_generate);
+            this.Controls.Add(this.lb_oszlopok);
+            this.Controls.Add(this.lb_sorok);
+            this.Controls.Add(this.tb_oszlopok);
+            this.Controls.Add(this.tb_sorok);
+            this.Controls.Add(this.pan_keret);
+            this.Name = "Form1";
+            this.Text = "Élet Játéka";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pan_keret;
+        private System.Windows.Forms.TextBox tb_sorok;
+        private System.Windows.Forms.TextBox tb_oszlopok;
+        private System.Windows.Forms.Label lb_sorok;
+        private System.Windows.Forms.Label lb_oszlopok;
+        private System.Windows.Forms.Button btn_generate;
     }
 }
 
--- GameOfLife/GameOfLife/Form1.cs
+++ GameOfLife/GameOfLife/Form1.cs
@@ -12,9 +12,88 @@ namespace GameOfLife
 {
     public partial class Form1 : Form
     {
+        Random rnd = new Random();
         public Form1()
-        {
+        {           
             InitializeComponent();
+        }
+
+        private void btn_generate_Click(object sender, EventArgs e)
+        {
+            int oszlop;
+            int sorok;
+            try
+            {
+                oszlop = int.Parse(tb_oszlopok.Text);
+                sorok = int.Parse(tb_sorok.Text);
+                if (oszlop <= 0 || sorok <= 0)
+                {
+                    MessageBox.Show("Az értékek legalább legyenek 1-ek");
+                    return;
+                }
+            }catch(Exception ex)
+            {
+                MessageBox.Show("nem megfelelő bemeneti érték");
+                return;
+            }
+
+            if(oszlop >= 25 || sorok >= 25)
+            {               
+                if (MessageBox.Show("Eléggé nagy számokat írtál be.\nEgy ideig eltarthat a legenerálás. Biztos vagy benne?", "Igen?", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+                {
+                    return;
+                }
+            }
+
+            PictureBox[,] fuvek_kepek = new PictureBox[sorok, oszlop];
+            pan_keret.Size = new Size(oszlop * 50, sorok * 50);
+
+            for (int i = 0; i < oszlop; i++)
+            {
+                for (int j = 0; j < sorok; j++) 
+                { 
+                    PictureBox kep = new PictureBox();
+                    pan_keret.Controls.Add(kep);
+                    kep.Size = new Size(50, 50);
+                    kep.BorderStyle = BorderStyle.FixedSingle;
+                    kep.Location = new Point(i * 50, j * 50);
+                    kep.Visible = true;
+                    switch (rnd.Next(1,4))
+                    {
+                        case 1:
+                            kep.BackColor = Color.Green;
+                            break;
+                        case 2:
+                            kep.BackColor = Color.Red;
+                            break;
+                        case 3:
+                            kep.BackColor = Color.Blue;
+                            break;
+                        default:
+                            kep.BackColor = Color.Transparent;
+                            break;
+                    }
+                    fuvek_kepek[j,i] = kep;
+                }
+            }
+
+            PictureBox[,] allatok_kepek = new PictureBox[sorok, oszlop];
+
+            for (int i = 0; i < oszlop; i++)
+            {
+                for (int j = 0; j < sorok; j++)
+                {
+                    PictureBox kep = new PictureBox();
+                    pan_keret.Controls.Add(kep);
+                    kep.Size = new Size(50, 50);
+                    kep.BorderStyle = BorderStyle.FixedSingle;
+                    kep.Location = new Point(i * 50, j * 50);
+                    kep.Visible = true;
+                    kep.BackColor = fuvek_kepek[j,i].BackColor;
+                    kep.BringToFront();
+                    allatok_kepek[j, i] = kep;
+                }
+            }
         }
     }
 }
